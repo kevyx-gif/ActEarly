8007d2318bef257fe7e4fb9fe8756bc59303e1b4
kevyx-gif <apolortms@gmail.com>
2023-07-19 10:50:19 -0300

    Proyecto base 0.1

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:actearly/pages/main_screens/screen1.dart';
+import 'package:actearly/pages/main_screens/screen2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
+    return MaterialApp(
+      title: 'ActEarly',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: <Widget>[
+          _buildPage(),
+          Screen1(),
+          Screen2(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _currentIndex =
+                0; // Establecer el índice a 0 para volver a la página principal
+          });
+        },
+        child: Icon(Icons.home),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 8.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.people),
+              onPressed: () {
+                setState(() {
+                  _currentIndex =
+                      1; // Establecer el índice a 1 para ir a la Página 1
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {
+                setState(() {
+                  _currentIndex =
+                      2; // Establecer el índice a 2 para ir a la Página 2
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPage() {
+    return Center(
+      child: Text('Página Inicial'),
+    );
+  }
+}
