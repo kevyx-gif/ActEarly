ec0a6b59bc9a4b7ddfffbc53df0c1c560a4aa325
kevyx-gif <apolortms@gmail.com>
2023-07-25 12:59:27 -0300

    0.4.3

--- lib/pages/another_pages/terms&cond.dart
+++ lib/pages/another_pages/terms&cond.dart
@@ -6,6 +6,8 @@ import 'package:get/get.dart';
 //futures import
 import 'package:actearly/utils/futures.dart';
 
+enum ButtonState { init, loading, done }
+
 class TermsAndConditionsScreen extends StatefulWidget {
   const TermsAndConditionsScreen({Key? key}) : super(key: key);
 
@@ -15,8 +17,14 @@ class TermsAndConditionsScreen extends StatefulWidget {
 }
 
 class TermsAndConditionsScreenState extends State<TermsAndConditionsScreen> {
+  bool isAnimating = true;
+  ButtonState state = ButtonState.init;
+
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final isStretched = isAnimating || state == ButtonState.init;
+    final isDone = state == ButtonState.done;
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -30,13 +38,13 @@ class TermsAndConditionsScreenState extends State<TermsAndConditionsScreen> {
           SizedBox(height: 5),
           Expanded(
             child: Card(
-              margin: const EdgeInsets.all(20),
+              margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(30.0),
               ),
               child: Padding(
                 padding:
-                    const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
                 child: Align(
                   alignment: Alignment.center,
                   child: SingleChildScrollView(
@@ -57,38 +65,58 @@ class TermsAndConditionsScreenState extends State<TermsAndConditionsScreen> {
             ),
           ),
           Container(
-            alignment: Alignment.bottomRight,
-            margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-            child: SizedBox.fromSize(
-              size: Size(56, 56), // button width and height
-              child: ClipOval(
-                child: Material(
-                  color: Colors.green, // button color
-                  child: InkWell(
-                      splashColor: Colors.lightGreen, // splash color
-                      onTap: () {
-                        acceptTermsAndConditionsAccepted();
-                      }, // button pressed
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(Icons.task_alt_rounded,
-                                color: Colors.white), // icon
-                            Text(
-                              "textAccept".tr,
-                              style: TextStyle(color: Colors.white),
-                            ), // text
-                          ],
-                        ),
-                      )),
-                ),
-              ),
-            ),
+            margin: EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+            padding: EdgeInsets.all(8),
+            child: Center(
+                child: AnimatedContainer(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 60),
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.easeIn,
+                    width: state == ButtonState.init ? width : 50,
+                    onEnd: () => setState(() => isAnimating = !isAnimating),
+                    height: 40,
+                    child: isStretched
+                        ? buildButton()
+                        : buildSmallButton(isDone))),
           )
         ],
       ),
     );
+  }
+
+  Widget buildButton() => Container(
+      child: OutlinedButton(
+          style: OutlinedButton.styleFrom(
+              shape: StadiumBorder(),
+              side: BorderSide(width: 2, color: Colors.blue)),
+          onPressed: () async {
+            setState(() => state = ButtonState.loading);
+            await Future.delayed(Duration(seconds: 3));
+            setState(() => state = ButtonState.done);
+            await Future.delayed(Duration(seconds: 3));
+            acceptTermsAndConditionsAccepted();
+          },
+          child: FittedBox(
+            child: Text('textAccept'.tr,
+                style: TextStyle(fontSize: 18, color: Colors.black)),
+          )));
+
+  Widget buildSmallButton(bool isDone) {
+    final color = isDone ? Colors.green : Colors.blue;
+
+    return Container(
+        decoration: BoxDecoration(shape: BoxShape.circle, color: color),
+        child: isDone
+            ? Icon(Icons.done, size: 30, color: Colors.white)
+            : Center(
+                child: SizedBox(
+                child: Center(
+                    child: CircularProgressIndicator(
+                  color: Colors.white,
+                  strokeWidth: 2,
+                )),
+                height: 30.0,
+                width: 30.0,
+              )));
   }
 }
