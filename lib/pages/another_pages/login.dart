8602c60cd9bc67d3ced02e4e55a30b278ffbed4f
Christian Hernandez <herc991207@gs.utm.mx>
2023-07-26 12:20:48 -0300

    0.4.8.2

--- lib/pages/another_pages/login.dart
+++ lib/pages/another_pages/login.dart
@@ -20,7 +20,6 @@ class _LoginPageState extends State<LoginPage> {
             Navigator.pushNamed(context, '/');
           }, child: const Text('Create')
           ),
-         //TextField()
       ),
     );
   }
