e3b951cf7144c4a7eaeedbd7b7c3f2a010af0c01
Christian Hernandez <herc991207@gs.utm.mx>
2023-08-04 14:42:35 -0300

    0.5.3

--- lib/pages/another_pages/register.dart
+++ lib/pages/another_pages/register.dart
@@ -1,17 +1,19 @@
 import "package:flutter/material.dart";
+import 'package:actearly/widgets/dropdown.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({ super.key });
 
   @override
   State<RegisterPage> createState() => _RegisterPageState();
+  
 }
 
 class _RegisterPageState extends State<RegisterPage> {
 
   final email = TextEditingController();
   final password = TextEditingController();
-
+  
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
@@ -20,9 +22,10 @@ class _RegisterPageState extends State<RegisterPage> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
+    List<String> list = <String>['Padre/Tutor Legal', 'Educador/Maestro', 'Usuario/Cuidador', 'Evaluador a domicilio'];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Register Page'),
@@ -41,7 +44,23 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
               ),
           ),
-        ],
+          Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextField(
+                  controller: password,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'contraseña',
+                  ),
+                ),
+              ),
+            ),
+          Center( 
+            child: DropdownButtonExample(list),   
+            
+          ),
+        ],  
       ),
     );
   }
