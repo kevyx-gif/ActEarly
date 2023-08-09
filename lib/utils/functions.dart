b046056393cb45966195d8a56a71d8594fe9247b
kevyx-gif <apolortms@gmail.com>
2023-08-09 11:52:01 -0300

    0.6.0

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -1,3 +1,4 @@
+import 'package:actearly/utils/colors.dart';
 import "package:flutter/material.dart";
 import 'package:actearly/utils/futures.dart';
 
@@ -7,79 +8,50 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'dart:io' show Platform;
 //Text Imports
 import 'package:get/get.dart';
-
-//login
-void login(BuildContext context, TextEditingController email,
-    TextEditingController password) async {
-  if (await searchByFieldInCollection('users', 'email', email.text)) {
-    //found password
-    if (await searchByFieldInCollection('users', 'password', password.text)) {
-      loggedIn();
-      setUserData(email.text);
-      Fluttertoast.showToast(
-        msg: "Welcome $email.text",
-        toastLength: Toast.LENGTH_SHORT,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.black,
-        textColor: Colors.white,
-        fontSize: 16.0,
-      );
-      Navigator.pushNamed(context, '/main');
-    } else {
-      if (Platform.isAndroid) {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            content: Text(
-          'wrongPassword'.tr,
-          textAlign: TextAlign.center,
-        )));
-      } else {
-        Fluttertoast.showToast(
-          msg: 'wrongPassword'.tr,
-          toastLength: Toast.LENGTH_SHORT,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.black,
-          textColor: Colors.white,
-          fontSize: 16.0,
-        );
-      }
-    }
-  } else {
-    if (Platform.isAndroid) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text(
-        'wrongEmail'.tr,
-        textAlign: TextAlign.center,
-      )));
-    } else {
-      Fluttertoast.showToast(
-        msg: 'wrongEmail'.tr,
-        toastLength: Toast.LENGTH_SHORT,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.black,
-        textColor: Colors.white,
-        fontSize: 16.0,
-      );
-    }
-  }
-}
+//colores
+import 'package:actearly/utils/colors.dart';
 
 //-----------------Menssage Toast Per-----------------//
-void messageToast(String msg, BuildContext context) {
+void messageToast(
+    BuildContext context, String msg, Color backgroundC, Color color) {
   if (Platform.isAndroid) {
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text(
-      'wrongPassword'.tr,
+      msg,
       textAlign: TextAlign.center,
     )));
   } else {
     Fluttertoast.showToast(
-      msg: 'wrongPassword'.tr,
+      msg: msg,
       toastLength: Toast.LENGTH_SHORT,
       timeInSecForIosWeb: 1,
-      backgroundColor: Colors.black,
-      textColor: Colors.white,
+      backgroundColor: backgroundC,
+      textColor: color,
       fontSize: 16.0,
     );
+  }
+}
+
+//login
+void login(BuildContext context, TextEditingController email,
+    TextEditingController password) async {
+  final emailUser = email.text;
+  Color negro = ColorConstants.black;
+  Color white = ColorConstants.white;
+  Color red = ColorConstants.red;
+
+  if (await searchByFieldInCollection('users', 'email', emailUser)) {
+    //found password
+    if (await searchByFieldInCollection('users', 'password', password.text)) {
+      loggedIn();
+      setUserData(emailUser);
+      messageToast(context, 'hola $emailUser ', negro, white);
+      Navigator.pushNamed(context, '/main');
+    } else {
+      messageToast(context, 'wrongPassword'.tr, red, white);
+    }
+  } else {
+    messageToast(context, 'wrongEmail'.tr, red, white);
   }
 }
 
@@ -87,5 +59,7 @@ void messageToast(String msg, BuildContext context) {
 void logOut(BuildContext context) {
   loggedOut();
   cleanUserData();
+  messageToast(
+      context, 'Cuenta cerrada', ColorConstants.red, ColorConstants.white);
   Navigator.pushNamed(context, '/login');
 }
