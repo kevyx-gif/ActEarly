ad1d2d48cccc1bcbf7594d55db1fb3572d7dbbe1
Christian Hernandez <herc991207@gs.utm.mx>
2023-08-11 16:35:51 -0300

    modificacion id

--- lib/pages/main_screens/screen2.dart
+++ lib/pages/main_screens/screen2.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Screen2 extends StatelessWidget {
-  const Screen2({super.key});
 
+  Screen2({super.key});
+
+  
   @override
   Widget build(BuildContext context) {
+
     return const Center(
-      child: Text('Contenido de la pantalla 2'),
+      child: Text('data'),
     );
   }
 }
