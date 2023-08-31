482e25df297cf4b3c1541da05e1f762e4ef095c7
Christian Hernandez <herc991207@gs.utm.mx>
2023-08-31 16:03:52 -0300

    Date casi listo

--- lib/pages/another_pages/dateChildren.dart
+++ lib/pages/another_pages/dateChildren.dart
@@ -36,6 +36,8 @@ class _DateChildrenState extends State<DateChildren> {
 
 import 'package:flutter/material.dart';
 
+import '../../utils/functions.dart';
+
 class DateChildrenA extends StatelessWidget {
   const DateChildrenA({super.key});
 
@@ -58,171 +60,95 @@ class DateChildren extends StatefulWidget {
 }
 
 class _DateChildrenState extends State<DateChildren> {
-  final _formKey = GlobalKey<FormState>();
-  String _name = '';
-  late DateTime _selectedDate;
-  late TimeOfDay _selectedTime;
-  var _description = '';
 
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime.now(),
-      lastDate: DateTime.now().add(Duration(days: 365)),
-    );
-    if (picked != null && picked != _selectedDate) {
-      setState(() {
-        _selectedDate = picked;
-      });
-    }
-  }
+  final GlobalKey<FormState> titleDateKey= GlobalKey<FormState>();
 
-  Future<void> _selectTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay.now(),
-    );
-    if (picked != null && picked != _selectedTime) {
-      setState(() {
-        _selectedTime = picked;
-      });
-    }
-  }
+  final _titleController = TextEditingController();
+  final _dateController = TextEditingController();
+  final _timeController = TextEditingController();
+  final _descriptionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Agendar Cita'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Nombre'),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Por favor ingresa tu nombre';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _name = value!;
-                },
-              ),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: () => _selectDate(context),
-                      child: AbsorbPointer(
-                        child: TextFormField(
-                          decoration: InputDecoration(labelText: 'Fecha'),
-                          controller: TextEditingController(
-                            text: _selectedDate != null
-                                ? "${_selectedDate.toLocal()}".split(' ')[0]
-                                : '',
-                          ),
-                          validator: (value) {
-                            if (_selectedDate == null) {
-                              return 'Por favor selecciona una fecha';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: () => _selectTime(context),
-                      child: AbsorbPointer(
-                        child: TextFormField(
-                          decoration: InputDecoration(labelText: 'Hora'),
-                          controller: TextEditingController(
-                            text: _selectedTime != null
-                                ? "${_selectedTime.format(context)}"
-                                : '',
-                          ),
-                          validator: (value) {
-                            if (_selectedTime == null) {
-                              return 'Por favor selecciona una hora';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Descripción'),
-                maxLines: 3,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Por favor ingresa una descripción';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _description = value!;
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState?.save();
-                    // Aquí podrías guardar la cita en una base de datos o realizar otras acciones
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text('Cita Agendada'),
-                          content: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Nombre: $_name'),
-                              Text('Fecha: ${_selectedDate.toLocal()}'),
-                              Text('Hora: ${_selectedTime.format(context)}'),
-                              Text('Descripción: $_description'),
-                            ],
-                          ),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: Text('Cerrar'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  }
-                },
-                child: Text('Agendar Cita'),
-              ),
-            ],
+    return Form(
+      key: titleDateKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            controller: _titleController,
+            decoration: const InputDecoration(
+              hintText: 'Title:',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
           ),
-        ),
+          
+          TextFormField(
+            controller: _dateController,
+            decoration: const InputDecoration(
+              hintText: 'Date',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+
+          TextFormField(
+            controller: _timeController,
+            decoration: const InputDecoration(
+              hintText: 'Time',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+
+          TextFormField(
+            controller: _descriptionController,
+            decoration: const InputDecoration(
+              hintText: 'Description',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (titleDateKey.currentState!.validate()) {
+                  updateChildDatabase();
+                  // Process data.
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
       ),
     );
   }
-  
 }
 
 /* dentro de class_DateChildrenState
-final GlobalKey<FormState> titleDateKey= GlobalKey<FormState>();
+  final GlobalKey<FormState> titleDateKey= GlobalKey<FormState>();
   final GlobalKey<FormState> dateKey = GlobalKey<FormState>();
   final GlobalKey<FormState> timeDateKey= GlobalKey<FormState>();
   final GlobalKey<FormState> descriptionDateKey = GlobalKey<FormState>();
